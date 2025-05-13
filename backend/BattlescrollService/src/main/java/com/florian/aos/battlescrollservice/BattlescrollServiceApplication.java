8db0a5390b46883bbaf8b085e6ceb38bc91c6ff0
Florian <floriandelafosse@gmail.com>
2025-05-13 15:08:41 +0200

    initialisation battleScroll service

---
+++ backend/BattlescrollService/src/main/java/com/florian/aos/battlescrollservice/BattlescrollServiceApplication.java
@@ -0,0 +1,13 @@
+package com.florian.aos.battlescrollservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BattlescrollServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BattlescrollServiceApplication.class, args);
+	}
+
+}
---
+++ backend/BattlescrollService/src/test/java/com/florian/aos/battlescrollservice/BattlescrollServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian.aos.battlescrollservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattlescrollServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
