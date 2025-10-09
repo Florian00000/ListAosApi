5cbbaecaaacbf3c75ade172b11713d4fd2596c34
FlorianD <floriandelafosse@gmail.com>
2025-10-09 14:44:52 +0200

    initialisation container MySql poru BattleScroll Service

--- sql/battlescroll_service.sql
+++ sql/battlescroll_service.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `battlescroll_service` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `battlescroll_service`;
 -- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
 --
 -- Host: localhost    Database: battlescroll_service
@@ -452,4 +454,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-07 16:37:52
+-- Dump completed on 2025-10-09 12:21:16
