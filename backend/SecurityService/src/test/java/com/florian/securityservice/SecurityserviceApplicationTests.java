07b5981b63d05256763fd5d9bc0c52becb9ee21c
FlorianD <floriandelafosse@gmail.com>
2025-10-27 12:08:27 +0100

    initialisation security service

---
+++ backend/SecurityService/src/test/java/com/florian/securityservice/SecurityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.florian.securityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityserviceApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
